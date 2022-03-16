74305d97861c840e4b281a4ca286f954653178c5
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2022-03-16 19:17:05 +0100

    fetch hook

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,27 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  async fetch() {
+    await this.getMovies()
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=19dbe41caa3710ddd5b7382da5b29c0b&language=en-US&page=1`
+      )
+      const result = await data
+      result.data.results.forEach((movie) => {
+        this.movies.push(movie)
+      })
+    },
+  },
 }
 </script>
